25c2245c7513b2b45c7d94bb178924c7d14499f0
MultiApollo19 <mariusz.belwon@gmail.com>
2023-02-23 14:29:30 +0100

    OG test

--- app/head.js
+++ app/head.js
@@ -3,8 +3,17 @@ export default function Head() {
     <>
       <title>Launcher</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="NONE" />
+      <meta name="description" content="Provide users most recent version of our games!" />
       <link rel="icon" href="/favicon.ico" />
+      <meta property="og:title" content="Launcher" />
+      <meta
+        property="og:description"
+        content="Provide users most recent version of our games!"
+      />
+      <meta
+        property="og:image"
+        content="https://cklczqohgdctyqulkssz.supabase.co/storage/v1/object/public/updates/0.1.0/Baner.png"
+      />
     </>
   )
 }
