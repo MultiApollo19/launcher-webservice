108a98a83d5118da2f0fa88578eadff47f739b1c
MultiApollo19 <mariusz.belwon@gmail.com>
2023-02-23 16:51:01 +0100

    font fix sign-in

--- app/sign-in/page.js
+++ app/sign-in/page.js
@@ -36,7 +36,7 @@ export default function SignIn() {
                     <label className='mr-60 text-white text-xl flex group-focus/email:text-[#2596FF]'>Sign in with email</label>
                     <input type='text' id='email' name='email' required  className='bg-[#242B48] rounded flex focus:outline-none text-white focus:ring-[#1f80db] focus:ring focus:border-[#1f80db] focus:bg-[#3D4874]' />
                     <label className='mr-60 mt-8 text-white text-xl flex '>Password</label>
-                    <input type='password' id='password' name='password' required className=' bg-[#242B48] rounded flex focus:outline-none text-whit focus:ring-[#1f80db] focus:ring focus:border-[#1f80db] focus:bg-[#3D4874]' />
+                    <input type='password' id='password' name='password' required className=' bg-[#242B48] rounded flex focus:outline-none text-white focus:ring-[#1f80db] focus:ring focus:border-[#1f80db] focus:bg-[#3D4874]' />
 
                     <button type="submit" className=' flex py-6 bg-[#2596FF] text-white text-xl w-40 h-9 mt-20 place-items-center rounded mx-auto justify-center hover:bg-[#1f80db]'><p>Sign in</p></button>
                     <Link href='/sign-up'><button className=' flex text-white text  h-9 mt-1 place-items-center rounded mx-auto justify-center hover:underline'><p>I don't have account</p></button></Link>
